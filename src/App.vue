285790e6a7f0e2b6ee51a222e70b6a4b99c08f41
ab.bouzidi <abdeldjalil.bouzidi@etu.usthb.dz>
2023-06-14 11:23:10 +0100

    v-if v-show v-once

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,18 @@
   >
     <h1
         class="text-5xl"
+        v-show="false"
     >
       Number signe
     </h1>
     <div
       class="flex gap-2"
     >
-      <p>The number {{ number }} is : </p>
+      <p
+
+      >
+        The number {{ number }} is :
+      </p>
       <p
           v-if="number > 0"
           class="font-semibold"
@@ -29,12 +34,37 @@
       >
         zero
       </p>
+<!--      <p-->
+<!--          v-show="number > 0"-->
+<!--          class="font-semibold"-->
+<!--      >-->
+<!--        positive-->
+<!--      </p>-->
+<!--      <p-->
+<!--          v-show="number < 0"-->
+<!--          class="font-semibold"-->
+<!--      >-->
+<!--        negative-->
+<!--      </p>-->
+<!--      <p-->
+<!--          v-eshow="number == 0"-->
+<!--          class="font-semibold"-->
+<!--      >-->
+<!--        zero-->
+<!--      </p>-->
     </div>
   </div>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 
-const number = ref(0)
+let msg = ref('')
+let number = ref(150)
+
+// if (number.value > 0) msg.value = 'positive'
+// else if(number.value < 0) msg.value = 'negative'
+// else msg.value = 'zero'
+
+
 </script>
