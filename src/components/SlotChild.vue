3e9caa0974a91d026f4da6ed6cf1fbfae71d900b
ab.bouzidi <abdeldjalil.bouzidi@etu.usthb.dz>
2023-06-20 10:17:59 +0100

    slots

---
+++ src/components/SlotChild.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+  <div
+    class="flex flex-col bg-gray-400 w-full items-center"
+  >
+    <h2 class="text-red-400 text-2xl">{{ title }}</h2>
+
+    <slot name="header" />
+    <slot name="body" />
+  </div>
+</template>
+
+
+<script setup>
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Title'
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
